c23ad90bb92f107ab47714b8cec8d6c00f5fcb66
LazuliKao <LazuliKao233@outlook.com>
2025-04-17 17:25:20 +0800

    init

---
+++ OpenMeshViewer/OpenMeshViewer.cpp
@@ -0,0 +1,375 @@
+﻿// OpenMeshViewer.cpp : Defines the entry point for the application.
+
+#include "OpenMeshViewer.h"
+#include <QApplication>
+#include <QMainWindow>
+#include <QMenuBar>
+#include <QStatusBar>
+#include <QVBoxLayout>
+#include <QMatrix4x4>
+#include <cmath>
+
+// Shader sources
+const char *vertexShaderSource = R"(
+    #version 330 core
+    layout(location = 0) in vec3 position;
+    layout(location = 1) in vec3 normal;
+    
+    uniform mat4 model;
+    uniform mat4 view;
+    uniform mat4 projection;
+    
+    out vec3 fragNormal;
+    out vec3 fragPos;
+    
+    void main() {
+        gl_Position = projection * view * model * vec4(position, 1.0);
+        fragNormal = mat3(transpose(inverse(model))) * normal;
+        fragPos = vec3(model * vec4(position, 1.0));
+    }
+)";
+
+const char *fragmentShaderSource = R"(
+    #version 330 core
+    in vec3 fragNormal;
+    in vec3 fragPos;
+    
+    out vec4 fragColor;
+    
+    void main() {
+        vec3 lightPos = vec3(10.0, 10.0, 10.0);
+        vec3 lightColor = vec3(1.0, 1.0, 1.0);
+        vec3 objectColor = vec3(0.7, 0.7, 0.9);
+        
+        // Ambient
+        float ambientStrength = 0.3;
+        vec3 ambient = ambientStrength * lightColor;
+        
+        // Diffuse
+        vec3 norm = normalize(fragNormal);
+        vec3 lightDir = normalize(lightPos - fragPos);
+        float diff = max(dot(norm, lightDir), 0.0);
+        vec3 diffuse = diff * lightColor;
+        
+        vec3 result = (ambient + diffuse) * objectColor;
+        fragColor = vec4(result, 1.0);
+    }
+)";
+
+// MeshViewerWidget implementation
+MeshViewerWidget::MeshViewerWidget(QWidget *parent)
+    : QOpenGLWidget(parent),
+      meshLoaded(false),
+      program(nullptr),
+      vertexBuffer(QOpenGLBuffer::VertexBuffer),
+      indexBuffer(QOpenGLBuffer::IndexBuffer),
+      rotationX(0.0f),
+      rotationY(0.0f),
+      zoom(5.0f),
+      indexCount(0)
+{
+    setFocusPolicy(Qt::StrongFocus);
+}
+
+MeshViewerWidget::~MeshViewerWidget()
+{
+    makeCurrent();
+
+    if (program)
+    {
+        delete program;
+        program = nullptr;
+    }
+    vao.destroy();
+    vertexBuffer.destroy();
+    indexBuffer.destroy();
+
+    doneCurrent();
+}
+
+bool MeshViewerWidget::loadMesh(const QString &filename)
+{
+    if (!OpenMesh::IO::read_mesh(mesh, filename.toStdString()))
+    {
+        return false;
+    }
+
+    // Update vertex normals
+    mesh.request_face_normals();
+    mesh.request_vertex_normals();
+    mesh.update_normals();
+
+    meshLoaded = true;
+
+    // If context is already available, update mesh buffers
+    if (context())
+    {
+        makeCurrent();
+        updateMeshBuffers();
+        doneCurrent();
+    }
+
+    resetView();
+    update();
+
+    return true;
+}
+
+void MeshViewerWidget::resetView()
+{
+    rotationX = 0.0f;
+    rotationY = 0.0f;
+    zoom = 5.0f;
+
+    modelMatrix.setToIdentity();
+    viewMatrix.setToIdentity();
+    viewMatrix.translate(0.0f, 0.0f, -zoom);
+
+    update();
+}
+
+void MeshViewerWidget::initializeGL()
+{
+    initializeOpenGLFunctions();
+    glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+    glEnable(GL_DEPTH_TEST);
+
+    // Create shader program
+    program = new QOpenGLShaderProgram();
+    program->addShaderFromSourceCode(QOpenGLShader::Vertex, vertexShaderSource);
+    program->addShaderFromSourceCode(QOpenGLShader::Fragment, fragmentShaderSource);
+    program->link();
+
+    // Create VAO and buffers
+    vao.create();
+    vao.bind();
+
+    vertexBuffer.create();
+    indexBuffer.create();
+
+    vao.release();
+
+    // If a mesh is already loaded, update buffers
+    if (meshLoaded)
+    {
+        updateMeshBuffers();
+    }
+}
+
+void MeshViewerWidget::updateMeshBuffers()
+{
+    if (!meshLoaded)
+        return;
+
+    vao.bind();
+
+    // Prepare vertex data (position and normal)
+    QVector<GLfloat> vertices;
+    for (Mesh::VertexIter v_it = mesh.vertices_begin(); v_it != mesh.vertices_end(); ++v_it)
+    {
+        Mesh::Point p = mesh.point(*v_it);
+        Mesh::Normal n = mesh.normal(*v_it);
+
+        // Position
+        vertices << p[0] << p[1] << p[2];
+        // Normal
+        vertices << n[0] << n[1] << n[2];
+    }
+
+    // Prepare index data for triangular faces
+    QVector<GLuint> indices;
+    for (Mesh::FaceIter f_it = mesh.faces_begin(); f_it != mesh.faces_end(); ++f_it)
+    {
+        for (Mesh::FaceVertexIter fv_it = mesh.fv_iter(*f_it); fv_it.is_valid(); ++fv_it)
+        {
+            indices << fv_it->idx();
+        }
+    }
+
+    // Store number of indices for later use
+    indexCount = indices.size();
+
+    // Upload vertex data
+    vertexBuffer.bind();
+    vertexBuffer.allocate(vertices.constData(), vertices.size() * sizeof(GLfloat));
+
+    // Set vertex attribute pointers
+    program->bind();
+
+    // Position attribute
+    int posAttr = program->attributeLocation("position");
+    program->enableAttributeArray(posAttr);
+    program->setAttributeBuffer(posAttr, GL_FLOAT, 0, 3, 6 * sizeof(GLfloat));
+
+    // Normal attribute
+    int normalAttr = program->attributeLocation("normal");
+    program->enableAttributeArray(normalAttr);
+    program->setAttributeBuffer(normalAttr, GL_FLOAT, 3 * sizeof(GLfloat), 3, 6 * sizeof(GLfloat));
+
+    // Upload index data
+    indexBuffer.bind();
+    indexBuffer.allocate(indices.constData(), indices.size() * sizeof(GLuint));
+
+    program->release();
+    vao.release();
+}
+
+void MeshViewerWidget::paintGL()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    if (!meshLoaded || !program)
+        return;
+
+    program->bind();
+    vao.bind();
+
+    // Update model matrix with current rotation
+    modelMatrix.setToIdentity();
+    modelMatrix.rotate(rotationX, 1.0f, 0.0f, 0.0f);
+    modelMatrix.rotate(rotationY, 0.0f, 1.0f, 0.0f);
+
+    // Update view matrix with current zoom
+    viewMatrix.setToIdentity();
+    viewMatrix.translate(0.0f, 0.0f, -zoom);
+
+    // Set uniforms
+    program->setUniformValue("model", modelMatrix);
+    program->setUniformValue("view", viewMatrix);
+    program->setUniformValue("projection", projectionMatrix);
+
+    // Draw mesh
+    glDrawElements(GL_TRIANGLES, indexCount, GL_UNSIGNED_INT, 0);
+
+    vao.release();
+    program->release();
+}
+
+void MeshViewerWidget::resizeGL(int width, int height)
+{
+    // Update projection matrix
+    projectionMatrix.setToIdentity();
+    projectionMatrix.perspective(45.0f, width / float(height), 0.1f, 100.0f);
+}
+
+void MeshViewerWidget::mousePressEvent(QMouseEvent *event)
+{
+    lastMousePosition = event->pos();
+}
+
+void MeshViewerWidget::mouseMoveEvent(QMouseEvent *event)
+{
+    if (event->buttons() & Qt::LeftButton)
+    {
+        QPoint delta = event->pos() - lastMousePosition;
+        rotationY += 0.5f * delta.x();
+        rotationX += 0.5f * delta.y();
+
+        update();
+    }
+
+    lastMousePosition = event->pos();
+}
+
+void MeshViewerWidget::wheelEvent(QWheelEvent *event)
+{
+    zoom -= event->angleDelta().y() / 120.0f;
+    zoom = qMax(1.0f, qMin(zoom, 15.0f));
+
+    viewMatrix.setToIdentity();
+    viewMatrix.translate(0.0f, 0.0f, -zoom);
+
+    update();
+}
+
+// MainWindow implementation
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    // Create central widget with mesh viewer
+    QWidget *centralWidget = new QWidget();
+    QVBoxLayout *layout = new QVBoxLayout(centralWidget);
+
+    meshViewer = new MeshViewerWidget();
+    layout->addWidget(meshViewer);
+
+    setCentralWidget(centralWidget);
+
+    // Create menus and actions
+    createActions();
+    createMenus();
+
+    // Set window properties
+    setWindowTitle("OpenMesh Viewer");
+    resize(800, 600);
+
+    // Set status bar message
+    statusBar()->showMessage("Ready");
+}
+
+void MainWindow::createActions()
+{
+    QMenu *fileMenu = menuBar()->addMenu("&File");
+
+    QAction *openAction = new QAction("&Open", this);
+    openAction->setShortcut(QKeySequence::Open);
+    connect(openAction, &QAction::triggered, this, &MainWindow::openFile);
+    fileMenu->addAction(openAction);
+
+    fileMenu->addSeparator();
+
+    QAction *exitAction = new QAction("E&xit", this);
+    exitAction->setShortcut(QKeySequence::Quit);
+    connect(exitAction, &QAction::triggered, this, &QWidget::close);
+    fileMenu->addAction(exitAction);
+
+    QMenu *helpMenu = menuBar()->addMenu("&Help");
+
+    QAction *aboutAction = new QAction("&About", this);
+    connect(aboutAction, &QAction::triggered, this, &MainWindow::about);
+    helpMenu->addAction(aboutAction);
+}
+
+void MainWindow::createMenus()
+{
+    // Menus are already created in createActions()
+}
+
+void MainWindow::openFile()
+{
+    QString filename = QFileDialog::getOpenFileName(this,
+                                                    "Open Mesh File", "", "Mesh Files (*.obj *.off *.stl *.ply);;All Files (*)");
+
+    if (filename.isEmpty())
+        return;
+
+    statusBar()->showMessage("Loading mesh...");
+
+    if (meshViewer->loadMesh(filename))
+    {
+        statusBar()->showMessage("Mesh loaded successfully", 3000);
+    }
+    else
+    {
+        QMessageBox::critical(this, "Error", "Failed to load mesh file");
+        statusBar()->showMessage("Failed to load mesh", 3000);
+    }
+}
+
+void MainWindow::about()
+{
+    QMessageBox::about(this, "About OpenMesh Viewer",
+                       "OpenMesh Viewer\n\n"
+                       "A simple 3D mesh viewer using Qt and OpenMesh\n"
+                       "© 2025");
+}
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    MainWindow mainWindow;
+    mainWindow.show();
+
+    return app.exec();
+}
---
+++ OpenMeshViewer/OpenMeshViewer.h
@@ -0,0 +1,85 @@
+﻿// OpenMeshViewer.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <string>
+
+// Qt includes
+#include <QMainWindow>
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions>
+#include <QOpenGLBuffer>
+#include <QOpenGLVertexArrayObject>
+#include <QOpenGLShaderProgram>
+#include <QMouseEvent>
+#include <QFileDialog>
+#include <QMessageBox>
+
+// OpenMesh includes
+#include <OpenMesh/Core/IO/MeshIO.hh>
+#include <OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh>
+
+// Define the mesh type
+typedef OpenMesh::TriMesh_ArrayKernelT<> Mesh;
+
+// MeshViewerWidget class - OpenGL widget for rendering the mesh
+class MeshViewerWidget : public QOpenGLWidget, protected QOpenGLFunctions
+{
+    Q_OBJECT
+
+public:
+    MeshViewerWidget(QWidget* parent = nullptr);
+    ~MeshViewerWidget();
+
+    bool loadMesh(const QString& filename);
+    void resetView();
+
+protected:
+    void initializeGL() override;
+    void paintGL() override;
+    void resizeGL(int width, int height) override;
+    
+    void mousePressEvent(QMouseEvent* event) override;
+    void mouseMoveEvent(QMouseEvent* event) override;
+    void wheelEvent(QWheelEvent* event) override;
+
+private:
+    void updateMeshBuffers();
+    
+    Mesh mesh;
+    bool meshLoaded;
+    
+    QOpenGLShaderProgram* program;
+    QOpenGLVertexArrayObject vao;
+    QOpenGLBuffer vertexBuffer;
+    QOpenGLBuffer indexBuffer;
+    int indexCount;  // Store the number of indices for drawing
+    
+    QMatrix4x4 modelMatrix;
+    QMatrix4x4 viewMatrix;
+    QMatrix4x4 projectionMatrix;
+    
+    QPoint lastMousePosition;
+    float rotationX, rotationY;
+    float zoom;
+};
+
+// MainWindow class - the application's main window
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget* parent = nullptr);
+
+private slots:
+    void openFile();
+    void about();
+
+private:
+    MeshViewerWidget* meshViewer;
+    void createActions();
+    void createMenus();
+};
